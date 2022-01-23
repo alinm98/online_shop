943c3c48d7c2110ebb4d1b0ea2b343d6178272f7
alinm98 <77275459+alinm98@users.noreply.github.com>
2022-01-23 20:48:39 +0330

    adding admin panel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('panelAdmin' , function (){
+    return view('Admin.index');
+});
