f1fe941bf31a20bf56ccd2ee498ddbb391fc8a78
alinm98 <77275459+alinm98@users.noreply.github.com>
2022-02-07 17:30:50 +0330

    adding client to project

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Client.layout.master');
 });
 
 Route::get('panelAdmin' , function (){
